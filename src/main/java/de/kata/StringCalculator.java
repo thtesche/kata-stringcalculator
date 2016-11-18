4fd5a87bdd64498203721257942f2d3749c9e6a1
thtesche <thomas@tesche.de>
2016-11-18 12:46:18 +0100

    Kata from 2016-11-18

--- src/main/java/de/kata/StringCalculator.java
+++ src/main/java/de/kata/StringCalculator.java
@@ -1,5 +1,10 @@
 package de.kata;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 /**
  * @author thtesche
  */
@@ -11,19 +16,19 @@ class StringCalculator {
     }
 
     int calc() throws NegativesNotAllowedException {
-        int sum = 0;
+
+        if (input.length() == 0) return 0;
+
+        return calculateSum();
+    }
+
+    private int calculateSum() throws NegativesNotAllowedException {
         StringBuilder exceptionMessage = new StringBuilder();
+        int sum = 0;
 
-        if (input.length() == 0) {
-            return sum;
-        }
-
-        String[] splittedNumbers = input.split("[,\n]");
-        for (String numberString : splittedNumbers) {
-            int number = Integer.valueOf(numberString);
+        for (Integer number : getSplittedNumbers()) {
             if (number < 0) {
-                exceptionMessage.append(numberString);
-                exceptionMessage.append(",");
+                exceptionMessage.append(number).append(",");
 
             } else if (number <= 1000) {
                 sum += number;
@@ -35,5 +40,11 @@ class StringCalculator {
             throw new NegativesNotAllowedException(message);
         }
         return sum;
+    }
+
+    private List<Integer> getSplittedNumbers() {
+        return Arrays.stream(input.split("[,\n]"))
+                .map(Integer::valueOf)
+                .collect(toList());
     }
 }
